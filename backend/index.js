f21f859b1fc49bd2ef1ce53d7110b7511b226152
jitu7033 <jraj41797@gmail.com>
2025-01-05 22:19:47 +0530

    admin register work

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const app = express();
 const Routes = require("./routes/route.js")
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 dotenv.config();
 
 app.use(express.json({ limit: '10mb' }));
