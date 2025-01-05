064c74ebd7bfefa099e5ee7e386d13762474a0c1
manasvi8274 <manasvi1238274@gmail.com>
2025-01-05 22:18:03 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// import mongoose from "mongoose";
 const express = require("express");
 const cors = require("cors")
 const mongoose = require("mongoose")
@@ -10,14 +11,26 @@ dotenv.config();
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
-app.use('/',Routes); 
+
+try {
+    mongoose.connect(
+        "mongodb+srv://manasvi1238274:Manasvi1238274@cluster0.p8zho.mongodb.net/classprojectdatabase"
+    );
+    console.log("Db Connection success")
+} catch (error) {
+    console.error("DB connection failed with error: ", error)
+}
+
+
+// mongoose.connect(process.env.MONGO_URL, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(console.log("Connected to MongoDB"))
+//     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+app.use('/', Routes);
 
 app.listen(PORT, () => {
     console.log(`Server started at port no. ${PORT}`)
