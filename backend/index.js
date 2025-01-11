037e0c7a8affa5d80568bc536148001ce48abe74
manasvi8274 <manasvi1238274@gmail.com>
2025-01-11 15:19:52 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -13,22 +13,22 @@ app.use(cors());
 
 
 
-try {
-    mongoose.connect(
-        "mongodb+srv://manasvi1238274:Manasvi1238274@cluster0.p8zho.mongodb.net/classprojectdatabase"
-    );
-    console.log("Db Connection success")
-} catch (error) {
-    console.error("DB connection failed with error: ", error)
-}
+// try {
+//     mongoose.connect(
+//         "mongodb+srv://manasvi1238274:Manasvi1238274@cluster0.p8zho.mongodb.net/classprojectdatabase"
+//     );
+//     console.log("Db Connection success")
+// } catch (error) {
+//     console.error("DB connection failed with error: ", error)
+// }
 
 
-// mongoose.connect(process.env.MONGO_URL, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })
-//     .then(console.log("Connected to MongoDB"))
-//     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
 app.use('/', Routes);
 
